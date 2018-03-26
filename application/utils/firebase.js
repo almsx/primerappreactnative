f49079db121d3ba41c483c09203991660abaaed9
Alberto Luebbert M <albertoluebbert@ideashappy.com>
2018-03-26 02:04:57 -0600

    Clase de configuracion FireBase

---
+++ application/utils/firebase.js
@@ -0,0 +1,8 @@
+export default firebaseConfig = {
+    apiKey: "AIzaSyB--kxpjC-OblWLRho3bmw2g9cgDIj5cyA",
+    authDomain: "reactnativefirebase-880d5.firebaseapp.com",
+    databaseURL: "https://reactnativefirebase-880d5.firebaseio.com",
+    projectId: "reactnativefirebase-880d5",
+    storageBucket: "reactnativefirebase-880d5.appspot.com",
+    messagingSenderId: "199883879397"
+  };
