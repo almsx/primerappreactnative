1cc04802a3103f969ca89a34915e6591fefd1395
Alberto Luebbert M <albertoluebbert@ideashappy.com>
2018-04-04 12:44:25 -0500

    Geolocalizacion

--- application/screens/Geolocation.js
+++ application/screens/Geolocation.js
@@ -1,23 +1,117 @@
 import React, { Component } from 'react';
-import {View, Alert, Text} from 'react-native';
+import {View, Alert, Text, StyleSheet, Dimensions, Platform, StatusBar} from 'react-native';
 import BackgroundImage from "../components/BackgroundImage";
 import AppButton from "../components/AppButton";
 import { NavigationActions } from 'react-navigation';
 import * as firebase from 'firebase';
+import { Constants, MapView, Location, Permissions} from 'expo';
 
-export default class Geolocation extends Component {    
+export default class Geolocation extends Component {
 
     static navigationOptions = {
         title: 'Geolocalización'
     };
 
-    render () {
-        return (
-            <BackgroundImage source={require('../../assets/imagen2.jpg')}>
-                <View style={{justifyContent: 'center', flex: 1}}>
-                    <Text>Geolocalización</Text>
-                </View>
-            </BackgroundImage>
+    state = {
+        location: null,
+        errorMessage: null,
+        mapRegion: {
+            latitude: 19.363900,
+            longitude: -99.202020,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+        }
+    };    
+
+    _handleMapRegionChange = mapRegion => {
+        this.setState({ mapRegion });
+    };
+
+    componentWillMount() {
+        if (Platform.OS === 'android' && !Constants.isDevice) {
+          this.setState({
+            errorMessage: 'Solo funciona en tu dispositivo :)',
+        });
+      } else {
+          this._getLocationAsync();
+      }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permiso de Acceso ha sido denegado.',
+    });
+  }
+
+  let location = await Location.getCurrentPositionAsync({});
+  this.setState({ location });
+};
+
+render() {
+
+    let text = 'Espere por favor..';
+    
+    if (this.state.errorMessage) {
+        text = this.state.errorMessage;
+    } else if (this.state.location) {
+
+        let p_latitude = JSON.stringify(this.state.location.coords.latitude);
+        let p_longitude = JSON.stringify(this.state.location.coords.longitude);
+        let p_todo = JSON.stringify(this.state.location);
+
+        text = "Latitud "+ p_latitude + " y Latitud " + p_longitude;
+        let gps = "Latitud "+ p_latitude + " y Latitud " + p_longitude + " en enteros "+parseFloat(p_latitude) + " y "+parseFloat(p_longitude);
+
+        /*
+        Alert.alert(
+          'POC Geolocalización', gps
         );
+        */
+
+        this.setState({
+            location: null,
+            errorMessage: null,
+            mapRegion: {
+                latitude: parseFloat(p_latitude),
+                longitude: parseFloat(p_longitude),
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421
+            }
+        });
+
+
+    }
+
+    return (
+        <View style={styles.container}>
+        { /* <Text style={styles.paragraph}>{text}</Text> */ }
+        
+            <MapView
+                style =  {{ alignSelf: 'stretch', height: Dimensions.get('window').height,
+                            width: Dimensions.get('window').width 
+                        }}
+                region = {this.state.mapRegion}
+                onRegionChange={this._handleMapRegionChange}
+            />
+            <StatusBar backgroundColor="blue" barStyle="light-content" />
+        </View>
+    );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: '#ecf0f1',
+    },
+    paragraph: {
+        margin: 24,
+        fontSize: 18,
+        textAlign: 'center',
+    }
+});
