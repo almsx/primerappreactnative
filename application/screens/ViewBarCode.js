356bcc804795513274294712996f2e3d278c9ef6
Alberto Luebbert M <albertoluebbert@ideashappy.com>
2018-04-04 01:09:30 -0500

    Vista Codigo de Barras

--- application/screens/ViewBarCode.js
+++ application/screens/ViewBarCode.js
@@ -1,23 +1,79 @@
 import React, { Component } from 'react';
-import {View, Alert, Text} from 'react-native';
+import {View, Alert, Text, StyleSheet, Dimensions} from 'react-native';
 import BackgroundImage from "../components/BackgroundImage";
 import AppButton from "../components/AppButton";
 import { NavigationActions } from 'react-navigation';
 import * as firebase from 'firebase';
 
+import { Constants, BarCodeScanner, Permissions } from 'expo';
+
 export default class ViewBarCode extends Component {    
+
 
     static navigationOptions = {
         title: 'Código de Barras'
     };
+    state = {
+        hasCameraPermission: null
+    };
 
-    render () {
-        return (
-            <BackgroundImage source={require('../../assets/imagen2.jpg')}>
-                <View style={{justifyContent: 'center', flex: 1}}>
-                    <Text>Código de Barras</Text>
-                </View>
-            </BackgroundImage>
-        );
+    componentDidMount() {
+        this._requestCameraPermission();
     }
-}
+
+    _requestCameraPermission = async () => {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({
+          hasCameraPermission: status === 'granted',
+      });
+    };
+
+    _handleBarCodeRead = data => {
+        let variable = JSON.stringify(data);
+        let stock = variable;
+        Alert.alert(
+          'Código de Barras',stock
+          );
+    };
+
+    render() {
+        const {width2} = Dimensions.get('window');
+        const {height2} = Dimensions.get('window');
+
+        console.log(width2);
+        
+        return (
+          <View style={styles.container}>
+          {this.state.hasCameraPermission === null ?
+              <Text>Solicitud de Permiso de Camara</Text> :
+              this.state.hasCameraPermission === false ?
+              <Text>El permiso de Camara no fue permitido</Text> :
+              <BarCodeScanner
+              torchMode="on"
+              onBarCodeRead={this._handleBarCodeRead}
+              style={{ height: Dimensions.get('window').height,
+              width: Dimensions.get('window').width }}
+              />
+          }
+          </View>
+          );
+      }
+  }
+
+  const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: '#ecf0f1',
+    },
+    paragraph: {
+        margin: 24,
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#34495e',
+    },
+});
+
